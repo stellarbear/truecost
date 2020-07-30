26ecd8cbddba4a90697a51956a549addff06f37e
stellarbear <stellarbears@gmail.com>
2020-07-30 17:54:47 +0200

    feat: email

---
+++ packages/backend/src/mail/samples/order.ts
@@ -0,0 +1,49 @@
+import {domain, linkUri} from "../helpers";
+import {header} from "../header";
+import {footer} from "../footer";
+
+export const orderEmail = (code: string, data: Record<string, string>) => {
+    const logo = linkUri('/default/logo.png');
+    const assistant = linkUri('/default/assistant.png');
+    const trackUrl = `${domain}/track`;
+
+    return `
+    <mjml>
+        ${header("Account verification", `${domain} registration`)}
+        <mj-body background-color="#E7E7E7" width="600px">
+            <mj-section full-width="full-width" background-color="#00695f" padding-bottom="0">
+                <mj-column width="100%">
+                    <mj-image src="${logo}" alt="" align="center" width="150px" />
+                </mj-column>
+                <mj-column width="100%" background-color="#ffffff" padding="15px" padding-bottom="0px">
+                    <mj-text color="#212b35" font-weight="bold" font-size="20px">
+                    Order receipt:
+                    </mj-text>
+                    <mj-text color="#637381" font-size="50px" padding-top="20px">
+                    <strong>${code}</strong>
+                    </mj-text>
+                    <ul>
+                    ${Object.keys(data).map(key => `<li style="padding-bottom: 20px"><strong>${key}:</strong> ${data[key]}`)}
+                    </ul>
+                </mj-column>
+            </mj-section>
+            <mj-wrapper padding-top="0" padding-bottom="0" css-class="body-section">
+                <mj-section background-color="#ffffff" padding-left="15px" padding-right="15px">
+                    <mj-column width="100%">
+                    <mj-divider border-color="#DFE3E8" border-width="1px" />
+                    </mj-column>
+                    <mj-column width="80%">
+                    <mj-text color="#637381" font-size="16px">
+                        Note: Use code and email to <a class="text-link" href="${trackUrl}">track your order</a>. Or just sign in to your account
+                    </mj-text>
+                    </mj-column>
+                    <mj-column width="20%">
+                    <mj-image width="100px" src="${assistant}" />
+                    </mj-column>
+                </mj-section>
+            </mj-wrapper>
+            ${footer()}
+        </mj-body>
+    </mjml>
+    `
+}
