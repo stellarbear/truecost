655f5d64bc1ff4497ef15278a7ddda90af4ef6ad
mikhail <mikhailchukhnin@gmail.com>
2020-08-17 10:39:53 +0300

    fix: 24 items for shop as default

--- packages/frontend/src/pages/Shop/Shop.tsx
+++ packages/frontend/src/pages/Shop/Shop.tsx
@@ -64,7 +64,7 @@ const Shop: React.FC = () => {
     )
 
     const renderItems = (data: string[]) => (
-        <ArraySlice data={data} prefix="shop-pagination" scroll={300}>
+        <ArraySlice data={data} prefix="shop-pagination" scroll={300} chunk={24}>
             {(itemIds => (
                 <RowGrid w={250} s={16} p={16}>
                     {itemIds.map(id => (
