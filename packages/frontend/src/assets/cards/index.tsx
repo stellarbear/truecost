6cea442ad4a2d2c75203ebd991968ed3e65de9f5
mikhail <mikhailchukhnin@gmail.com>
2020-09-18 12:46:51 +0300

    feat: payment cards

---
+++ packages/frontend/src/assets/cards/index.tsx
@@ -0,0 +1,7 @@
+export * from './amex';
+export * from './diners';
+export * from './discover';
+export * from './jcb';
+export * from './mastercard';
+export * from './unionpay';
+export * from './visa';
--- packages/frontend/src/pages/Navigation/Footer.tsx
+++ packages/frontend/src/pages/Navigation/Footer.tsx
@@ -4,6 +4,17 @@ import {Link} from "react-router-dom";
 import {social} from "auxiliary/social";
 import SocialDialog from "../Base/SocialDialog";
 import {Col, Row} from "pages/Base/Grid";
+import {Visa, MasterCard, Discover, JCB, Unionpay, Amex, Diners} from "assets/cards";
+
+const payment = [
+    Visa,
+    MasterCard,
+    Discover,
+    JCB,
+    Unionpay,
+    Amex,
+    Diners,
+];
 
 export const Footer: React.FC = () => (
     <Paper elevation={6} style={{
@@ -12,35 +23,53 @@ export const Footer: React.FC = () => (
         color: "#fff",
         backgroundColor: "#263238",
     }}>
-        <Grid container alignItems="center">
-            <Grid item xs={12} sm={5} md={3}>
-                <Col p={8}>
-                    <Button color="inherit" component={Link} to={"/about"}>About us</Button>
-                    <Button color="inherit" component={Link} to={"/policy"}>Privacy policy</Button>
-                    <Button color="inherit" component={Link} to={"/tos"}>Terms of Service</Button>
-                </Col>
-            </Grid>
-            <Grid item xs={12} sm={1} md={3} />
+        <Col fullWidth>
+            <Grid container alignItems="center" >
+                <Grid item xs={12} sm={5} md={3}>
+                    <Col p={8}>
+                        <Button color="inherit" component={Link} to={"/about"}>About us</Button>
+                        <Button color="inherit" component={Link} to={"/policy"}>Privacy policy</Button>
+                        <Button color="inherit" component={Link} to={"/tos"}>Terms of Service</Button>
+                    </Col>
+                </Grid>
+
+                <Grid item xs={12} sm={1} md={3} />
             <Grid item xs={12} sm={6} md={6}>
-                <Col fullWidth s={8} p={8} style={{marginTop: 8}}>
-                    <Typography variant="body2" color="inherit" align="center">© Truecost 2019-2020</Typography>
-                    <Typography variant="body2" color="inherit" align="center">2885 Sanford
+                    <Col fullWidth s={8} p={8} style={{marginTop: 8}}>
+                        <Typography variant="body2" color="inherit" align="center">© Truecost 2019-2020</Typography>
+                        <Typography variant="body2" color="inherit" align="center">2885 Sanford
                         Ave SW #46305 Grandville, MI 49418</Typography>
-                    <Row wrap>
-                        {
-                            social.map((item, index) => (
-                                <SocialDialog key={`social-${index}`} button={
-                                    (
-                                        <IconButton color="inherit" size="small">
-                                            {React.cloneElement(item.icon, {style: {color: "white"}})}
-                                        </IconButton>
-                                    )
-                                } {...item} />
-                            ))
-                        }
-                    </Row>
-                </Col>
+                        <Row wrap>
+                            {
+                                social.map((item, index) => (
+                                    <SocialDialog key={`social-${index}`} button={
+                                        (
+                                            <IconButton color="inherit" size="small">
+                                                {React.cloneElement(item.icon, {style: {color: "white"}})}
+                                            </IconButton>
+                                        )
+                                    } {...item} />
+                                ))
+                            }
+                        </Row>
+                    </Col>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={1}>
+
+                </Grid>
             </Grid>
-        </Grid>
+        </Col>
+        <Col style={{backgroundColor: "#E0E0E0"}}>
+            <Row s={8} style={{marginTop: 40, marginBottom: -8}}>
+                {payment.map((e, i) => React.createElement(e, {
+                    style: {
+                        width: 40,
+                        transform: "scale(3.0)",
+                    },
+                    key: i,
+                }))}
+            </Row>
+        </Col>
     </Paper>
 );
