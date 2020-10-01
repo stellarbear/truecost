e83cc2969392de6bd37e47f633511e37dadfd233
mikhail <mikhailchukhnin@gmail.com>
2020-10-01 12:25:52 +0300

    fix: 4

--- packages/frontend/src/auxiliary/sitemap.ts
+++ packages/frontend/src/auxiliary/sitemap.ts
@@ -16,28 +16,33 @@ export const generateSiteMap = (games: string[], items: {u: string; g: string}[]
     return (
         `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${basePages.map(page => (
-            `<url>
-                <loc>${frontend.uri}/${page}</loc>
-                <changefreq>monthly</changefreq>
-                <priority>0.7</priority>
-            </url>
-            `)).join('')}
-            ${games.map((game) => (
-            `<url>
-                <loc>${frontend.uri}/${game}</loc>
+            <url>
+                <loc>${frontend.uri}</loc>
                 <changefreq>always</changefreq>
                 <priority>1</priority>
              </url>
-           `))
-            .join('')}
-            ${items.map(({u, g}) => (
-            `<url>
-                <loc>${frontend.uri}/${g}/item/${u}</loc>
-                <changefreq>daily</changefreq>
-                <priority>0.8</priority>
-            </url>
-            `)).join('')}
+             ${games.map((game) => (
+             `<url>
+                 <loc>${frontend.uri}/${game}</loc>
+                 <changefreq>always</changefreq>
+                 <priority>0.9</priority>
+              </url>
+            `))
+             .join('')}
+             ${items.map(({u, g}) => (
+             `<url>
+                 <loc>${frontend.uri}/${g}/item/${u}</loc>
+                 <changefreq>daily</changefreq>
+                 <priority>0.8</priority>
+             </url>
+             `)).join('')}
+             ${basePages.map(page => (
+             `<url>
+                 <loc>${frontend.uri}/${page}</loc>
+                 <changefreq>monthly</changefreq>
+                 <priority>0.7</priority>
+             </url>
+             `)).join('')}
         </urlset>
         `
     );
