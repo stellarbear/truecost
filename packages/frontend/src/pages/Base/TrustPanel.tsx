3d7b2b9a1ca9e5545c8a461bf812c36084f1e7f5
mikhail <mikhailchukhnin@gmail.com>
2020-11-13 23:03:25 +0300

    fix: trustpilot

--- packages/frontend/src/pages/Base/TrustPanel.tsx
+++ packages/frontend/src/pages/Base/TrustPanel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ButtonBase, Hidden} from "@material-ui/core";
+import {ButtonBase, Hidden, NoSsr} from "@material-ui/core";
 import ChevronRight from '@material-ui/icons/ChevronRight';
 import {shuffle} from "@truecost/shared";
 import {Col, Row, RowSwipable} from "./Grid";
@@ -22,69 +22,62 @@ export const TrustPanel: React.FC<IProps> = ({style = {}}) => {
         .slice(0, 3).map(i => reviews[i]);
 
     return (
-        <div style={style}>
-            <a target="_blank" rel="noreferrer"
-                style={{
-                    width: "100%", height: "100%",
-                    textDecoration: "none",
-                    color: "#000",
-                    fontSize: 12,
-                    fontFamily: "'Roboto',Arial",
-                    fontWeight: 14,
-                }}
-                href={url}>
-                <ButtonBase>
-                    <Col>
-                        <Row fullWidth justify="space-between" style={{
-                            height: 32,
-                            paddingBottom: 13,
-                            borderBottom: "1px solid #000",
-                            marginBottom: 35,
+        <Col style={style}>
+            <NoSsr>
+                <Row fullWidth justify="space-between" style={{
+                    height: 32,
+                    paddingBottom: 13,
+                    borderBottom: "1px solid #000",
+                    marginBottom: 35,
+                }}>
+                    <a target="_blank" rel="noreferrer" href={url}
+                        style={{
+                            display: "contents",
+                            textDecoration: "none",
+                            color: "#000",
                         }}>
-                            <Row s={8} align="center">
-                                <img height={32} src="/tp/logo.svg" alt="Trustpilot" />
-                                <p style={{
-                                    paddingTop: 17,
-                                }}>TrustScore <strong>4.8 / 5</strong></p>
-                            </Row>
-                            <Hidden smDown>
-                                <Row align="center" style={{
-                                    marginTop: 12,
-                                }}>
-                                    <div
-                                        style={{
-                                            textTransform: "uppercase",
-                                            fontWeight: 800,
-                                            color: "#000",
-                                        }}>ALL reviews </div>
-                                    <ChevronRight />
-                                </Row>
-                            </Hidden>
+                        <Row s={8} align="center">
+                            <img height={32} src="/tp/logo.svg" alt="Trustpilot" />
+                            <p style={{
+                                paddingTop: 17,
+                            }}>TrustScore <strong>4.8 / 5</strong></p>
                         </Row>
-                        <RowSwipable id="tp" w={250} s={16}>
-                            {data.map(({who, text, title}, index) => (
-                                <Col key={index} align="flex-start">
-                                    <img height={32} src="/tp/stars.svg" alt="star" />
-                                    <p style={{
-                                        fontSize: 14,
-                                        fontFamily: "'Roboto',Arial",
-                                    }}><strong>{title}</strong></p>
-                                    <p style={{
-                                        wordBreak: "break-word",
-                                        fontSize: 13,
-                                        textAlign: "left",
-                                        fontFamily: "'Roboto',Arial",
-                                    }}>{text}</p>
-                                    <p style={{
-                                        fontSize: 14,
-                                        fontFamily: "'Roboto',Arial",
-                                    }}><strong>{who}</strong></p>
-                                </Col>
-                            ))}
-                        </RowSwipable>
-                    </Col>
-                </ButtonBase>
-            </a>
-        </div>
+                        <Hidden smDown>
+                            <Row align="center" style={{
+                                marginTop: 12,
+                            }}>
+                                <div
+                                    style={{
+                                        textTransform: "uppercase",
+                                        fontWeight: 800,
+                                    }}>ALL reviews </div>
+                                <ChevronRight />
+                            </Row>
+                        </Hidden>
+                    </a>
+                </Row>
+                <RowSwipable id="tp" w={250} s={16}>
+                    {data.map(({who, text, title}, index) => (
+                        <Col key={index} align="flex-start">
+                            <img height={32} src="/tp/stars.svg" alt="star" />
+                            <p style={{
+                                fontSize: 15,
+                                fontFamily: "'Roboto',Arial",
+                            }}><strong>{title}</strong></p>
+                            <p style={{
+                                wordBreak: "break-word",
+                                fontSize: 16,
+                                textAlign: "left",
+                                fontFamily: "'Roboto',Arial",
+                            }}>{text}</p>
+                            <p style={{
+                                fontSize: 15,
+                                fontFamily: "'Roboto',Arial",
+                            }}><strong>{who}</strong></p>
+                        </Col>
+                    ))}
+                </RowSwipable>
+            </NoSsr>
+        </Col>
     );
 };
