8b7f97eac2cc52050cdd3c5fd79e75c04a31823d
Mikhail Chukhnin <mikhailchukhnin@gmail.com>
2021-02-25 23:28:04 +0300

    fix: gtm

--- packages/frontend/src/html.tsx
+++ packages/frontend/src/html.tsx
@@ -54,11 +54,11 @@ const trustpilot = () => (
 
 const gtm = () => (
     <script dangerouslySetInnerHTML={{
-        __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P5HRH68');</script>`,
+        })(window,document,'script','dataLayer','GTM-P5HRH68');`,
     }} />
 )
 
